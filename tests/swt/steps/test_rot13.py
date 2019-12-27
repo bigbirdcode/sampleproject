85eaebb469ca70a193f86c4d4cb281a1fa6bb320
BigBird who likes to Code <peter.varhegyi@gmail.com>
2019-12-27 18:25:54 +0100

    Correct a misuse

--- tests/swt/steps/test_rot13.py
+++ tests/swt/steps/test_rot13.py
@@ -2,8 +2,6 @@
 
 # pragma pylint: disable=missing-docstring,redefined-outer-name
 
-from collections import namedtuple
-
 from pytest_bdd import scenarios, given, when, then, parsers
 
 import mysw.main
@@ -34,19 +32,19 @@ scenarios('../features')
 def given_my_sw_io(text_in):
     """Given steps works as fixture, it will return a storage,
     simulating io for my_sw"""
-    my_sw_io = namedtuple('MySwIo', ['text', 'ans'])
-    my_sw_io.text = text_in
-    my_sw_io.ans = ""
+    my_sw_io = dict()
+    my_sw_io["text"] = text_in
+    my_sw_io["ans"] = ""
     return my_sw_io
 
 
 @when('I encrypt it')
 @when('I decrypt it')
 def i_en_de_crypt(given_my_sw_io):
-    given_my_sw_io.ans = mysw.main.rot13(given_my_sw_io.text)
+    given_my_sw_io["ans"] = mysw.main.rot13(given_my_sw_io["text"])
 
 
 @then('I get the answer "<text_out>"')
 @then(parsers.parse('I get the answer "{text_out}"'))
 def i_get_answer(text_out, given_my_sw_io):
-    assert given_my_sw_io.ans == text_out
+    assert given_my_sw_io["ans"] == text_out
